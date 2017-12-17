560231cfff3cd165b94aa9fdde5f40da4d4c27c3
Adrian Matejov <a.matejov@centrum.sk>
2017-12-17 13:30:20 +0100

    sample requests `type` option
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- sample_requests.sh
+++ sample_requests.sh
@@ -2,15 +2,16 @@
 # TODO: get computer's ip (preferably from eth0)
 
 EXERCISE_NUM=1
-USR=${1:-`whoami`}
-HOSTNAME=${2:-`hostname`}
+TYPE=$1
+USR=${2:-`whoami`}
+HOSTNAME=${3:-`hostname`}
 
-echo "Sending start of the exercise..."
-wget --header="Content-Type: application/json" --post-data '{"type": "start", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '"`date +%s`"', "exercise_number": '$EXERCISE_NUM', "ip": "192.168.0.102"}' http://localhost:3000/gta -O /dev/null
-
-#echo "Sending sample command..."
-wget --header="Content-Type: application/json" --post-data '{"type": "command", "user": "'$USR'", "hostname": "'`$HOSTNAME`'", "date": '"`date +%s`"', "exercise_number": '$EXERCISE_NUM', "ip": "192.168.0.102", "level": 1, "command": "ls -l"}' http://localhost:3000/gta -O /dev/null
-
-#echo "Sending passing command..."
-
-#echo "Sending exit command..."
+if [ "$TYPE" = "start" ]; then
+    wget --header="Content-Type: application/json" --post-data '{"type": "start", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '"`date +%s`"', "exercise_number": '$EXERCISE_NUM', "ip": "192.168.0.102"}' http://localhost:3000/gta -O /dev/null
+elif [ "$TYPE" = "command" ]; then
+    wget --header="Content-Type: application/json" --post-data '{"type": "command", "user": "'$USR'", "hostname": "'`$HOSTNAME`'", "date": '"`date +%s`"', "exercise_number": '$EXERCISE_NUM', "ip": "192.168.0.102", "level": 1, "command": "ls -l"}' http://localhost:3000/gta -O /dev/null
+elif [ "$TYPE" = "passed" ]; then
+    wget --header="Content-Type: application/json" --post-data '{"type": "passed", "user": "'$USR'", "hostname": "'`$HOSTNAME`'", "date": '"`date +%s`"', "exercise_number": '$EXERCISE_NUM', "ip": "192.168.0.102", "level": 1, "command": "ls -l"}' http://localhost:3000/gta -O /dev/null
+elif [ "$TYPE" = "exit" ]; then
+    wget --header="Content-Type: application/json" --post-data '{"type": "exit", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '"`date +%s`"', "exercise_number": '$EXERCISE_NUM', "ip": "192.168.0.102"}' http://localhost:3000/gta -O /dev/null
+fi
