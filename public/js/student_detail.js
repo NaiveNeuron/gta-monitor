5802c9ba71ef8c93b365c8ac0520942b5237db65
Adrian Matejov <a.matejov@centrum.sk>
2018-02-18 18:51:05 +0100

    update: passed command is now green
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- public/js/student_detail.js
+++ public/js/student_detail.js
@@ -13,6 +13,8 @@ $(document).on('click', '.user-box', function(e) {
         //       Green color for command that passed the level
         if (post.type == 'command') {
             $('.modal-command-history').append('<code>$ ' + post.command + '</code>');
+        } else if (post.type == 'passed') {
+            $('.modal-command-history').append('<code class="passed-command">$ ' + post.command + '</code>');
         } else if (post.type == 'start') {
             $('.modal-started-at').text(get_date_from_string(post.date));
         } else if (post.type == 'exit') {
