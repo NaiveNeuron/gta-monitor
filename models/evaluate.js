9d857bac2237face176678d0e51ff58614e8e98f
Adrian Matejov <a.matejov@centrum.sk>
2018-02-24 21:43:51 +0100

    evaluation: double precision for score
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- models/evaluate.js
+++ models/evaluate.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
         },
 
         score: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.DOUBLE,
             allowNull: false,
             validate: {
                 notEmpty: true,
