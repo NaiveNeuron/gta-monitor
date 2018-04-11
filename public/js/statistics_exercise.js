068c82ad34c915689a938c5a4360b4cc1135b30e
Adrian Matejov <a.matejov@centrum.sk>
2018-04-11 21:02:09 +0200

    normalize passed/failed commands graph
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- public/js/statistics_exercise.js
+++ public/js/statistics_exercise.js
@@ -6,6 +6,7 @@ function STExercise()
 
 
 STExercise.prototype.increment_or_set = function(dict, level, type) {
+    level = level.match(/l[0-9]+/g) + '*';
     if (!(level in dict))
         dict[level] = {'passed': 0, 'command': 0};
     dict[level][type] += 1;
@@ -38,7 +39,7 @@ STExercise.prototype.initialize_level_histogram = function() {
                 {
                     label: 'Passed',
                     data: Object.values(this.level_commands).map(function(item) {
-                        return item[POST_PASSED];
+                        return item[POST_PASSED] / (item[POST_PASSED]+item[POST_COMMAND]);
                     }),
                     backgroundColor: 'rgba(58, 225, 55, 0.7)',
                     borderColor: 'rgba(54, 162, 235, 1)',
@@ -47,7 +48,7 @@ STExercise.prototype.initialize_level_histogram = function() {
                 {
                     label: 'Failed',
                     data: Object.values(this.level_commands).map(function(item) {
-                        return item[POST_COMMAND];
+                        return item[POST_COMMAND] / (item[POST_PASSED]+item[POST_COMMAND]);
                     }),
                     backgroundColor: 'rgba(225, 58, 55, 0.7)',
                     borderColor: 'rgba(54, 162, 235, 1)',
