bd6131f0bc9fc1554b5e45751e86216c0bc4068a
Adrian Matejov <a.matejov@centrum.sk>
2018-04-28 21:38:34 +0200

    computing md5 hash in sample_requests
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- sample_requests.sh
+++ sample_requests.sh
@@ -19,14 +19,17 @@ IP=${5:-"158.195.28.190"}
 LVL="l01"
 CMD=$(urlencode "cat file | grep something | awk blablabla")
 
+CONCATED="i"$EXERCISE_NUM"j%!d(string="$LVL")k"$HOME"l"
+HASH=$(echo -n $CONCATED | md5sum | cut -d " " -f 1)
+
 if [ "$TYPE" = "start" ]; then
     wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "start", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '`date +%s`', "exercise_number": "'$EXERCISE_NUM'", "ip": "'$IP'"}' $SERVER/gta -O /dev/null
 elif [ "$TYPE" = "command" ]; then
     wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "command", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '`date +%s`', "exercise_number": "'$EXERCISE_NUM'", "ip": "'$IP'", "level": "'$LVL'", "command": "'$CMD'"}' $SERVER/gta -O /dev/null
 elif [ "$TYPE" = "passed" ]; then
-    wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "passed", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '`date +%s`', "exercise_number": "'$EXERCISE_NUM'", "ip": "'$IP'", "level": "'$LVL'", "command": "'$CMD'", "homedir": "'$HOME'", "hash": "da886a7e66f4c87b2a21badf4ee97394"}' $SERVER/gta -O /dev/null
+    wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "passed", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '`date +%s`', "exercise_number": "'$EXERCISE_NUM'", "ip": "'$IP'", "level": "'$LVL'", "command": "'$CMD'", "homedir": "'$HOME'", "hash": "'$HASH'"}' $SERVER/gta -O /dev/null
 elif [ "$TYPE" = "exit" ]; then
-    wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "exit", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '`date +%s`', "exercise_number": "'$EXERCISE_NUM'", "ip": "'$IP'", "homedir": "'$HOME'", "hash": "da886a7e66f4c87b2a21badf4ee97394"}' $SERVER/gta -O /dev/null
+    wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "exit", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '`date +%s`', "exercise_number": "'$EXERCISE_NUM'", "ip": "'$IP'", "homedir": "'$HOME'", "hash": "'$HASH'"}' $SERVER/gta -O /dev/null
 elif [ "$TYPE" = "help" ]; then
     wget --no-check-certificate --header="Content-Type: application/json" --post-data '{"type": "help", "user": "'$USR'", "hostname": "'$HOSTNAME'", "date": '"`date +%s`"', "exercise_number": "'$EXERCISE_NUM'", "level": "'$LVL'", "ip": "'$IP'"}' $SERVER/gta -O /dev/null
 elif [ "$TYPE" = "ack" ]; then
