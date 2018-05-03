c5b9d11e3c7af0a68dc513356147630b3531930a
Adrian Matejov <a.matejov@centrum.sk>
2018-05-03 19:23:54 +0200

    update: logging errors in err handler
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- app.js
+++ app.js
@@ -162,6 +162,9 @@ app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+    // log error
+    console.error(err);
+
     // render the error page
     res.status(err.status || 500);
     res.render('error');
